81372d4917545e16659d6159971158c0ec5c3205
Mohammed Alkindi <alkndymhmd692@gmail.com>
2026-08-25 21:11:31 +0400

    fix: exclude public from eslint's typescript rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist/**', 'node_modules/**', 'site/**'] },
+  { ignores: ['dist/**', 'node_modules/**', 'public/**'] },
   ...tseslint.configs.recommended,
   {
     rules: {
